63d03c0c7c44586ffc1575ab112bf43c38f8b904
TimIsOverpowered <admin@angelthump.com>
2020-05-07 03:04:11 +0000

    better 404 handling

--- src/app.js
+++ src/app.js
@@ -60,7 +60,9 @@ app.configure(services);
 app.configure(channels);
 
 // Configure a middleware for 404s and the error handler
-app.use(express.notFound());
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
 app.use(express.errorHandler({ logger }));
 
 app.hooks(appHooks);
