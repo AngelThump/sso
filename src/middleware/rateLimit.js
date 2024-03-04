64d19aee5167d51421faa9d26de74a3d13048195
TimIsOverpowered <admin@angelthump.com>
2024-03-03 23:50:03 -0600

    add back rate limit bypass

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -1,5 +1,12 @@
 module.exports.limiter = (app) => {
   return async function (req, res, next) {
+    if (
+      req.headers["rate-limit-bypass"] ===
+      app.get("authentication").RATE_LIMIT_BYPASS
+    ) {
+      return next();
+    }
+
     app
       .get("rateLimiter")
       .consume(req.get("X-Real-IP") || req.ip)
